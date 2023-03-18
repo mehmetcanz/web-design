f6136d9f2e50ce93edbfeb4ba8eaf249e6ca7f37
mehmetcanz <mehemtcanz@gmail.com>
2023-03-18 16:37:38 +0300

    first commit

---
+++ js/template.js
@@ -0,0 +1,11 @@
+// Dark mode 
+function dark_light() {
+    let darkData = document.body.classList.toggle("dark_mode_css")
+}
+
+
+//now date 
+let now=()=>{
+document.getElementById("now_date").innerHTML=new Date().getFullYear();
+}
+now()
